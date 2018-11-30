d5d7bcf0432d5867358955f344adb1200a2edf5b
brucenelm <brucenelm@gmail.com>
2018-11-30 15:50:28 +0300

    created the get_red_flags and test_get_red_flags

---
+++ test.py
@@ -0,0 +1,57 @@
+import unittest
+import run
+import json
+from flask_testing import TestCase
+import datetime
+
+class TestMainFlask(TestCase):
+
+	def create_app(self):
+		return run.app
+
+	def test_get_red_flags(self):
+		response = self.client.get('/api/v1/red-flags')
+
+		data = json.loads(response.data.decode())
+		self.assertEqual(response.status_code, 200)
+		self.assertNotEqual(len(data['data']),0)
+
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	unittest.main()
